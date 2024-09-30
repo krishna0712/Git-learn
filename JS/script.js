fbfac0a98dc6611e71a07e69548d68dd8f41fc7d
krishna0712 <krishnaacharya072007@gmail.com>
2024-10-01 00:15:33 +0530

    Learning js day1

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+/*  1.  Intro 
+
+    console.log("Hello");
+    console.log("I like Pizza too");
+
+    window.alert("Hey this is an emergency !!")
+
+    This is a Program
+
+    document.getElementById("head1").textContent = "Hello Sir"
+    document.getElementById("para").textContent = "Have a Good Morning"
+*/
+
+let price = 10.99;
+let age;
+age = 41;
+let favfood = "Momos"
+
+
+console.log(`You are ${age} years old`);
+console.log(`Price is  ${price} rupees`);
+console.log(typeof favfood)
+
+let online = true;
+let forSale = false;
+
+console.log(`Bro is online:${online}`);
+console.log(`Is this car for sale:${forSale}`);
